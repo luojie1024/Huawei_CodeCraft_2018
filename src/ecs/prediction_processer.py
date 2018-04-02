7f5d15523f9981668a6c528d4c118eebe6ecd783
Joahluo <luojie@hnu.edu.cn>
2018-04-02 13:05:52 +0800

    add mode 12

--- src/ecs/prediction_processer.py
+++ src/ecs/prediction_processer.py
@@ -40,16 +40,17 @@ def predict_all(caseInfo):
     # 2016 4 5月份
     pos_time1 = datetime.strptime('2016-04-08 00:00:00', "%Y-%m-%d %H:%M:%S")
     pos_time2 = datetime.strptime('2016-04-15 00:00:00', "%Y-%m-%d %H:%M:%S")
-    pos_time3 = datetime.strptime('2016-04-18 00:00:00', "%Y-%m-%d %H:%M:%S")
-    pos_time4 = datetime.strptime('2016-04-19 00:00:00', "%Y-%m-%d %H:%M:%S")
+    pos_time3 = datetime.strptime('2016-04-16 00:00:00', "%Y-%m-%d %H:%M:%S")
+    pos_time4 = datetime.strptime('2016-04-18 00:00:00', "%Y-%m-%d %H:%M:%S")
     pos_time5 = datetime.strptime('2016-04-20 00:00:00', "%Y-%m-%d %H:%M:%S")
 
     if end_time == pos_time1:#样例1  2016-04-08
         predict_func = predict_model.model1_used_func  # 76.68
     elif end_time >= pos_time2 and end_time < pos_time3:
         predict_func = predict_model.model2_used_func  # 76.147
-    # elif end_time >= pos_time3 and end_time < pos_time4 :
-    #     predict_func = predict_model.model3_used_func # 76.94
+
+    elif end_time >= pos_time3 and end_time < pos_time4 :
+        predict_func = predict_model.model3_used_func # 76.94
     # elif end_time >= pos_time4 and end_time < pos_time5:
     #     predict_func = predict_model.model4_used_func # 76.052
 
