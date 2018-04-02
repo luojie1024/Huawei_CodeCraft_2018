259f44971a3524f06ff23dcc83ace59f986b87ac
Joahluo <luojie@hnu.edu.cn>
2018-04-02 12:47:23 +0800

    add mode 12

--- src/ecs/prediction_processer.py
+++ src/ecs/prediction_processer.py
@@ -38,8 +38,8 @@ def predict_all(caseInfo):
     start_time = datetime.strptime(caseInfo.data_range[0], "%Y-%m-%d %H:%M:%S")
     end_time = datetime.strptime(caseInfo.data_range[1], "%Y-%m-%d %H:%M:%S")
     # 2016 4 5月份
-    pos_time1 = datetime.strptime('2016-04-01 00:00:00', "%Y-%m-%d %H:%M:%S")
-    pos_time2 = datetime.strptime('2016-04-05 00:00:00', "%Y-%m-%d %H:%M:%S")
+    pos_time1 = datetime.strptime('2016-04-05 00:00:00', "%Y-%m-%d %H:%M:%S")
+    pos_time2 = datetime.strptime('2016-04-08 00:00:00', "%Y-%m-%d %H:%M:%S")
     pos_time3 = datetime.strptime('2016-04-10 00:00:00', "%Y-%m-%d %H:%M:%S")
     pos_time4 = datetime.strptime('2016-04-20 00:00:00', "%Y-%m-%d %H:%M:%S")
     pos_time5 = datetime.strptime('2016-04-20 00:00:00', "%Y-%m-%d %H:%M:%S")
