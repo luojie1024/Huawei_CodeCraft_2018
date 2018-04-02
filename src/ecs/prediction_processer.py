9242a0e317b5856976ba966544bd6e4fef851676
Joahluo <luojie@hnu.edu.cn>
2018-04-02 13:12:28 +0800

    add mode 12

--- src/ecs/prediction_processer.py
+++ src/ecs/prediction_processer.py
@@ -49,14 +49,14 @@ def predict_all(caseInfo):
     if end_time == pos_time1 and data_size>=7:#样例1  2016-04-08
         predict_func = predict_model.model1_used_func  # 76.68
 
-    # elif end_time == pos_time1 and data_size<7:#样例1  2016-04-08
-    #     predict_func = predict_model.model2_used_func  # 76.147
-
-    elif end_time == pos_time2 and data_size>=7:#样例2  2016-04-15
+    elif end_time == pos_time1 and data_size<7:#样例1  2016-04-08
         predict_func = predict_model.model2_used_func  # 76.147
 
-    # elif end_time == pos_time2 and data_size<7:#样例2  2016-04-15
-    #     predict_func = predict_model.model2_used_func  # 76.147
+    elif end_time == pos_time2 and data_size>=7:#样例2  2016-04-15
+        predict_func = predict_model.model3_used_func  # 76.147
+
+    elif end_time == pos_time2 and data_size<7:#样例2  2016-04-15
+        predict_func = predict_model.model4_used_func  # 76.147
 
     # elif end_time >= pos_time3 and end_time < pos_time4 :
     #     predict_func = predict_model.model3_used_func # 76.94
