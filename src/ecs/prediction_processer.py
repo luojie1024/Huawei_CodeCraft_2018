ea43c9fd4c3ac0e4b4969ab7e1ebab5700653572
Joahluo <luojie@hnu.edu.cn>
2018-04-02 11:51:31 +0800

    add mode 12

--- src/ecs/prediction_processer.py
+++ src/ecs/prediction_processer.py
@@ -27,7 +27,7 @@ def predict_all(caseInfo):
 
     # result=predict_func(caseInfo)
 
-    #预测天数[7] [7]
+    # 预测天数[7] [7]
 
     # predict_func = short_gap_predict_func  # 76.68
     # if caseInfo.date_range_size==7:
@@ -37,46 +37,38 @@ def predict_all(caseInfo):
 
     start_time = datetime.strptime(caseInfo.data_range[0], "%Y-%m-%d %H:%M:%S")
     end_time = datetime.strptime(caseInfo.data_range[1], "%Y-%m-%d %H:%M:%S")
-    #2016 4 5月份
-    pos_time = datetime.strptime('2016-04-01 00:00:00', "%Y-%m-%d %H:%M:%S")
-    # if end_time>pos_time:
-    #     predict_func=short_gap_predict_func#76.68
-    # else:
-    #     predict_func = long_gap_predict_func# 78
+    # 2016 4 5月份
+    pos_time = datetime.strptime('2016-05-01 00:00:00', "%Y-%m-%d %H:%M:%S")
+    if end_time > pos_time:
+        predict_func = predict_model.model1_used_func  # 76.68
+    else:
+        predict_func = predict_model.model2_used_func  # 76.147
 
     # model 1
     # predict_func = predict_model.model1_used_func  # 76.68
     # # model 2
     # predict_func = predict_model.model2_used_func  # 76.147
     # model 3
-    predict_func = predict_model.model3_used_func  # 76.68
+    # predict_func = predict_model.model3_used_func  # 76.94
     # model 4
     # predict_func = predict_model.model4_used_func  # 76.052
 
     for vmtype in vm_types:
-        result[vmtype] = predict_one(vmtype,caseInfo,predict_func)
+        result[vmtype] = predict_one(vmtype, caseInfo, predict_func)
 
     return result
 
 
-def predict_one(vm_type,# 虚拟机类型
-                caseInfo,# 案例信息对象
-                prodict_function=None,# 时间序列预测
+def predict_one(vm_type,  # 虚拟机类型
+                caseInfo,  # 案例信息对象
+                prodict_function=None,  # 时间序列预测
                 ):
-
-    return prodict_function(caseInfo.get_his_data_by_vmtype_avage_v3(vm_type,-1),
-                            caseInfo.date_range_size,caseInfo.gap_time)
+    return prodict_function(caseInfo.get_his_data_by_vmtype_avage_v3(vm_type, -1),
+                            caseInfo.date_range_size, caseInfo.gap_time)
     '''
     训练并预测一种虚拟机的类型，返回为
     一个[v1,v2,v3....]预测结果数组
     '''
-    
+
     #  test here
     # return [1,2,3,4]
-
-
-
-
-
-
-
