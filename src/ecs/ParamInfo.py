605b7e02bb707a8d391ad5864dbb7957ae71b0b9
Joahluo <luojie@hnu.edu.cn>
2018-04-05 17:11:41 +0800

    add recoder

--- src/ecs/ParamInfo.py
+++ src/ecs/ParamInfo.py
@@ -39,7 +39,7 @@ VM_TYPE_MODIFY1 = {#flavor1 1 flavor2 12 flavor8 -3
 
 #L2 type_size=5
 VM_TYPE_MODIFY2 = {#flavor1 1 flavor2 1 flavor3 1
-    'flavor1': 1, 'flavor2': 0, 'flavor3': 1, 'flavor4': 0, 'flavor5': 0,
+    'flavor1': 1, 'flavor2': 1, 'flavor3': 0, 'flavor4': 0, 'flavor5': 0,
     'flavor6': 0, 'flavor7': 0, 'flavor8': 0, 'flavor9': 0, 'flavor10': 0,
     'flavor11': 0, 'flavor12': 0, 'flavor13': 0, 'flavor14': 0, 'flavor15': 1
 }
