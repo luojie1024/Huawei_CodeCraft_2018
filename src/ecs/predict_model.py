42c2d672010163eca939a68fde8cb7f807e6f727
Joahluo <luojie@hnu.edu.cn>
2018-04-07 12:48:27 +0800

    add recoder

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -797,7 +797,7 @@ def predict_model15(his_data,  # 某种类型的虚拟机的历史数据
     n = 3  # 边长数
     # sigma = 0.5
 
-    back_week = 1
+    back_week = 3
     chis_data = copy.deepcopy(his_data['value'])
     cal_len = len(chis_data)
 
