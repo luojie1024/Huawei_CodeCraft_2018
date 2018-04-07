e5d33fe713a90116a944f6899a410d138df5f26f
Joahluo <luojie@hnu.edu.cn>
2018-04-07 12:51:11 +0800

    add recoder

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -842,7 +842,7 @@ def predict_model16(his_data,  # 某种类型的虚拟机的历史数据
        his_data:['time':[时间标签],'value':[值]]
        '''
 
-    n = 2  # 边长数
+    n = 3  # 边长数
     sigma = 0.5
 
     back_week = 3
