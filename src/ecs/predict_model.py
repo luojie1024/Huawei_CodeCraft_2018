4bf632df773eed5fb6b66fb51d08d77b07e3e21a
Joahluo <luojie@hnu.edu.cn>
2018-04-08 18:08:22 +0800

    add recoder

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -313,14 +313,13 @@ def predict_model8(his_data,  # 某种类型的虚拟机的历史数据
         his_data:['time':[时间标签],'value':[值]]
         '''
 
-    n =7  # 边长数
+    n =10  # 边长数
     sigma = 0.5
 
+    beta=2.0
     back_week = 1
     chis_data = copy.deepcopy(his_data['value'])
     cal_len = len(chis_data)
-
-    temp_result=0.0
 
     result = []
     for rept in range(date_range_size):  # 预测天数范围
@@ -349,10 +348,9 @@ def predict_model8(his_data,  # 某种类型的虚拟机的历史数据
         # noise = random.gauss(0, sigma)
         # noise = math.fabs(noise)
         # day_avage = int(math.ceil(day_avage + noise))
-        # day_avage = int(math.ceil(day_avage))
+        day_avage = int(math.ceil(day_avage))
         chis_data.append(day_avage)
-        temp_result+=day_avage
-    result.append(int(math.ceil(temp_result)))
+        result.append(day_avage)
 
     return result
 
