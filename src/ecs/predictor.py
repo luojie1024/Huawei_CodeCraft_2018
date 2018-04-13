694d6a868f1a73169ae44e34b18200d98d8ad49d
Joahluo <luojie@hnu.edu.cn>
2018-04-13 13:04:14 +0800

    smooth

--- src/ecs/predictor.py
+++ src/ecs/predictor.py
@@ -32,7 +32,7 @@ try_result = {}
 is_deeplearing = False
 use_smooth = True
 use_search_maximum = True
-use_pm_average = True
+use_pm_average = False
 
 
 def predict_vm(ecs_lines, input_lines, input_test_file_array=None):
