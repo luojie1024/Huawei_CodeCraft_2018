2326b51f782461607995e74533e26e54c4d787c2
Joahluo <luojie@hnu.edu.cn>
2018-04-14 20:51:22 +0800

    smooth

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -14,251 +14,280 @@ from const_map import *
 is_noise = 0
 
 
-def predict_model1(his_data,  # 某种类型的虚拟机的历史数据
-                   date_range_size, vm_type):  # 需要预测的长度
+def predict_model1(his_data, date_range_size, vm_type):  # 简单滑动平均法
+    '''
+    预测方案 1 指数滑动平均
+    :param his_data: 真实的历史数据出现次数表
+    :param date_range_size: 需要预测的长度
+    :return: 返回结果
+    '''
 
-    '''
-    预测方案一,使用MV模型预测，最近n天 
-    历史长度为n个粒度时间，权重设定暂定，
-    his_data:['time':[时间标签],'value':[值]]
-    '''
-    n = 7  # 历史长度
-    # 权重，从最近到最久，长度为n
-    ws = [0.45, 0.25,
-          0.15, 0.08,
-          0.04, 0.02,
-          0.01]
+    # sigma = 0.5
+
+    # 衰减值0.21
+    alpha = 0.21
+    # 历史天数
     chis_data = copy.deepcopy(his_data['value'])
-    result = []
-    for rept in range(date_range_size):
-        cal_len = len(chis_data)
-        tmpn = 0
-        predict = 0.0
-        for i in range(cal_len - 1, -1, -1):
-            predict += chis_data[i] * ws[tmpn]
-            tmpn += 1
-            if tmpn == n: break
-        predict = int(math.floor(predict))
-        chis_data.append(predict)
-        result.append(predict)
-    return result
-
-
-def predict_model2(his_data,  # 某种类型的虚拟机的历史数据
-                   date_range_size, vm_type):  # 需要预测的长度
-
-    '''
-    预测方案二,使用SMV 模型预测， 最近n天
-    历史长度为n个粒度时间，权重设定暂定，
-    his_data:['time':[时间标签],'value':[值]]
-    '''
-    n = 14  # 历史长度
-    # 权重，从最近到最久，长度为n
-
-    chis_data = copy.deepcopy(his_data['value'])
-    result = []
-    for rept in range(date_range_size):
-        cal_len = len(chis_data)
-        tmpn = 0
-        predict = 0.0
-        for i in range(cal_len - 1, -1, -1):
-            predict += chis_data[i]
-            tmpn += 1
-            if tmpn == n: break
-        # TODO 2018 3 21 Fix ZeroDivisionError: float division by zero
-        if tmpn != 0:
-            predict = int(math.ceil(predict * 1.0 / tmpn))
-        else:
-            predict = 0
-        chis_data.append(predict)
-        result.append(predict)
-    return result
-
-
-def predict_model3(his_data,  # 某种类型的虚拟机的历史数据
-                   date_range_size, vm_type):  # 需要预测的长度
-
-    '''
-    预测方案3,使用MV 模型预测，最近n天
-    历史长度为n个粒度时间，使用倒数权重，
-    his_data:['time':[时间标签],'value':[值]]
-    '''
-    n = 10  # 历史长度
-    # 权重，从最近到最久，长度为n
-    ws = []
-    ws_sum = 0.0
-    for i in range(n):
-        # tmp = 1.0/(1.0+i)
-        tmp = math.exp(-1.0 * i)
-        ws_sum += tmp
-        ws.append(tmp)
-    for i in range(n):
-        ws[i] = ws[i] / ws_sum
-
-    chis_data = copy.deepcopy(his_data['value'])
-    result = []
-    for rept in range(date_range_size):
-        cal_len = len(chis_data)
-        tmpn = 0
-        predict = 0.0
-        for i in range(cal_len - 1, -1, -1):
-            predict += chis_data[i] * ws[tmpn]
-            tmpn += 1
-            if tmpn == n: break
-        predict = int(math.ceil(predict))
-        chis_data.append(predict)
-        result.append(predict)
-    return result
-
-
-def predict_model4(his_data,  # 某种类型的虚拟机的历史数据
-                   date_range_size, vm_type):  # 需要预测的长度
-
-    '''
-    预测方案四,使用SMV 模型预测，添加正态随机噪声  最近n天
-    历史长度为n个粒度时间，权重设定暂定，
-    his_data:['time':[时间标签],'value':[值]]
-    '''
-    n = 13  # 历史长度
-    sigma = 0.4
-
-    chis_data = copy.deepcopy(his_data['value'])
-    result = []
-    for rept in range(date_range_size):
-        cal_len = len(chis_data)
-        tmpn = 0
-        predict = 0.0
-        for i in range(cal_len - 1, -1, -1):
-            predict += chis_data[i]
-            tmpn += 1
-            if tmpn == n: break
-        noise = random.gauss(0, sigma)
-        predict = int(math.ceil(predict * 1.0 / tmpn + noise))
-        chis_data.append(predict)
-        result.append(predict)
-    return result
-
-
-def front_out(w, b, x):
-    suma = 0.0
-    for i in range(len(w)):
-        suma += w[i] * x[i]
-    return suma + b[0]
-
-
-def change(w, b, x, py_y, lr):
-    for i in range(len(w)):
-        w[i] -= py_y * x[i] * lr
-    b[0] -= lr * py_y
-
-
-def predict_model5(his_data,  # 某种类型的虚拟机的历史数据
-                   date_range_size, vm_type):  # 需要预测的长度
-
-    '''
-    预测方案5,进行一次差分，然后使用MA处理   失败
-    历史长度为n个粒度时间，使用倒数权重，
-    his_data:['time':[时间标签],'value':[值]]
-    '''
-    n = 10  # 历史长度
-    sigma = 0.1
-    # 权重，从最近到最久，长度为n
-    lr = 0.01
-
-    origin_data = his_data['value']
-    chis_data = copy.deepcopy(origin_data)
-
-    # 差分,计算平均
-    diff = []
-    avag = []
-    for i in range(1, len(chis_data)):
-        b = origin_data[i]
-        a = origin_data[i - 1]
-        diff.append(b - a)
-        avag.append((a + b) / 2.0)
-    avag.append(origin_data[-1])
-
-    print 'diff', diff
-    print 'avag', avag
-    result = []
-
-    w = []
-    for i in range(n):
-        w.append(random.gauss(0, 0.1))
-    b = [random.gauss(0, 0.1)]
-
+    # 历史天数
     cal_len = len(chis_data)
-    for i in range(n, cal_len):
-        x = chis_data[i - n:i]
-        py = front_out(w, b, x)
-        y = chis_data[i]
-        #         print py,y
-        #         print w,b
-        change(w, b, x, py - y, lr)
+    temp_reuslt = 0.0
+    result = []
 
-    for rept in range(date_range_size):
-        cal_len = len(chis_data)
-        tmpn = 0
-        predict = 0.0
-        x = chis_data[cal_len - n:]
-        y = []
-        predict = front_out(w, b, x)
-        # change(w,b,x,predict-y,lr)
+    for rept in range(date_range_size):  # 预测天数范围
+        temp_value = 0.0
+        # 遍历预测值
+        for i in range(len(chis_data)):
+            temp_value += chis_data[len(chis_data) - i - 1] * alpha * pow(1 - alpha, i)
+        chis_data.append(temp_value)
+        temp_reuslt += temp_value
 
-        noise = random.gauss(0, sigma)
-        predict = int(math.ceil(predict * 1.0 + noise))
-        diff.append(predict)
-        predict = predict + chis_data[-1]
-        # chis_data.append(predict+chis_data[-1])
-        result.append(predict)
+    # noise = random.gauss(0, sigma)
+    # noise = math.fabs(noise)
+    # # 求一个浮点数的地板，就是求一个最接近它的整数 ceil向上取整
+    # result.append(int(math.floor(temp_reuslt+noise)))
+    result.append(int(math.floor(temp_reuslt)))
+    return result
+
+def predict_model2(his_data, date_range_size, vm_type):
+    '''
+    预测方案 2 指数滑动平均
+    :param his_data: 真实的历史数据出现次数表
+    :param date_range_size: 需要预测的长度
+    :return: 返回结果
+    '''
+
+    # sigma = 0.5
+
+    # 衰减值0.21
+    alpha = 0.21
+    # 历史天数
+    chis_data = copy.deepcopy(his_data['value'])
+    # 历史天数
+    cal_len = len(chis_data)
+    temp_reuslt = 0.0
+    result = []
+
+    for rept in range(date_range_size):  # 预测天数范围
+        temp_value = 0.0
+        # 遍历预测值
+        for i in range(len(chis_data)):
+            temp_value += chis_data[len(chis_data) - i - 1] * alpha * pow(1 - alpha, i)
+        chis_data.append(temp_value)
+        temp_reuslt += temp_value
+
+    # noise = random.gauss(0, sigma)
+    # noise = math.fabs(noise)
+    # # 求一个浮点数的地板，就是求一个最接近它的整数 ceil向上取整
+    # result.append(int(math.floor(temp_reuslt+noise)))
+    result.append(int(math.floor(temp_reuslt)))
     return result
 
 
-def predict_model6(his_data,  # 某种类型的虚拟机的历史数据
-                   date_range_size, vm_type):  # 需要预测的长度
-
+def predict_model3(his_data, date_range_size, vm_type):  # 简单滑动平均法
     '''
-    预测方案六,使用SMV模型预测，添加正态随机噪声 
-    先将数据进行一次平均处理，然后采用平均预测 
-    历史长度为n个粒度时间，权重设定暂定，
-    his_data:['time':[时间标签],'value':[值]]
+    预测方案 3 指数滑动平均
+    :param his_data: 真实的历史数据出现次数表
+    :param date_range_size: 需要预测的长度
+    :return: 返回结果
     '''
-    # 如果当前主机类型在当前数据集未出现
-    if (his_data['time'] == 0):
-        return [0]
 
-    n = 7  # 历史长度
-    sigma = 0.1
+    # sigma = 0.5
 
-    n_layer1 = 1
+    # 衰减值0.21
+    alpha = 0.21
+    # 历史天数
     chis_data = copy.deepcopy(his_data['value'])
+    # 历史天数
     cal_len = len(chis_data)
-    avag = []
-    tmp = 0.0
-    last = 0.0
-    for i in range(n_layer1):
-        last = chis_data[i] * 1.0 / n_layer1
-        tmp += last
-    avag.append(tmp)
-    for i in range(n_layer1, cal_len):
-        tmp -= last
-        last = chis_data[i] * 1.0 / n_layer1
-        tmp += last
-        avag.append(tmp)
-
+    temp_reuslt = 0.0
     result = []
-    for rept in range(date_range_size):
-        cal_len = len(avag)
-        tmpn = 0
-        predict = 0.0
-        for i in range(cal_len - 1, -1, -1):
-            predict += avag[i]
-            tmpn += 1
-            if tmpn == n: break
-        noise = random.gauss(0, sigma)
-        predict = int(math.ceil(predict * 1.0 / tmpn + noise))
-        avag.append(predict)
-        result.append(predict)
+
+    for rept in range(date_range_size):  # 预测天数范围
+        temp_value = 0.0
+        # 遍历预测值
+        for i in range(len(chis_data)):
+            temp_value += chis_data[len(chis_data) - i - 1] * alpha * pow(1 - alpha, i)
+        chis_data.append(temp_value)
+        temp_reuslt += temp_value
+
+    # noise = random.gauss(0, sigma)
+    # noise = math.fabs(noise)
+    # # 求一个浮点数的地板，就是求一个最接近它的整数 ceil向上取整
+    # result.append(int(math.floor(temp_reuslt+noise)))
+    result.append(int(math.floor(temp_reuslt)))
+    return result
+
+
+def predict_model4(his_data, date_range_size, vm_type):  # 霍尔特线性趋势法
+    '''
+    预测方案 4 霍尔特线性趋势法
+    :param his_data: 真实的历史数据出现次数表
+    :param date_range_size: 需要预测的长度
+    :return: 返回结果
+    '''
+    # 历史天数
+    chis_data = copy.deepcopy(his_data['value'])
+    # 历史天数
+    cal_len = len(chis_data)
+    temp_reuslt = 0.0
+    result = []
+    #
+    sigma = 0.2
+
+    # 衰减值
+    alpha = 0.2
+    # 趋势
+    beta = 0.0
+    # 权重 75.21
+    h = 1.75
+
+    y_hot_t = 0.0
+    l_t = 0.2
+    b_t = 0.2
+
+    # 初始trend
+    pre_b_t = 0.0
+    # 初始化level
+    pre_l_t = 0.0
+
+    for rept in range(date_range_size):  # 预测天数范围
+
+        # 遍历历史记录
+        for i in range(1, len(chis_data)):  # t+1开始
+            # 更新level trend
+            pre_l_t = l_t
+            pre_b_t = b_t
+            # step1 computer level
+            l_t = alpha * chis_data[i - 1] + (1 - alpha) * (pre_l_t + pre_b_t)
+            # step2 computer trend
+            b_t = beta * (l_t - pre_l_t) + (1 - beta) * b_t
+            # step3
+            y_hot_t = l_t + h * b_t
+            if y_hot_t < 0:
+                y_hot_t = 0
+        # 追加到历史表中
+        chis_data.append(y_hot_t)
+        # 保存结果
+        temp_reuslt += y_hot_t
+    noise = random.gauss(0, sigma)
+    noise = math.fabs(noise)
+    # 求一个浮点数的地板，就是求一个最接近它的整数 ceil向上取整
+    result.append(int(math.floor(temp_reuslt) + noise))
+    return result
+
+def predict_model5(his_data, date_range_size, vm_type):  # 霍尔特线性趋势法
+    '''
+    预测方案 十 霍尔特线性趋势法
+    :param his_data: 真实的历史数据出现次数表
+    :param date_range_size: 需要预测的长度
+    :return: 返回结果
+    '''
+    # 历史天数
+    chis_data = copy.deepcopy(his_data['value'])
+    # 历史天数
+    cal_len = len(chis_data)
+    temp_reuslt = 0.0
+    result = []
+    #
+    sigma = 0.2
+
+    # 衰减值
+    alpha = 0.2
+    # 趋势
+    beta = 0.0
+    # 权重 75.21
+    h = 1.75
+
+    y_hot_t = 0.0
+    l_t = 0.2
+    b_t = 0.2
+
+    # 初始trend
+    pre_b_t = 0.0
+    # 初始化level
+    pre_l_t = 0.0
+
+    for rept in range(date_range_size):  # 预测天数范围
+
+        # 遍历历史记录
+        for i in range(1, len(chis_data)):  # t+1开始
+            # 更新level trend
+            pre_l_t = l_t
+            pre_b_t = b_t
+            # step1 computer level
+            l_t = alpha * chis_data[i - 1] + (1 - alpha) * (pre_l_t + pre_b_t)
+            # step2 computer trend
+            b_t = beta * (l_t - pre_l_t) + (1 - beta) * b_t
+            # step3
+            y_hot_t = l_t + h * b_t
+            if y_hot_t < 0:
+                y_hot_t = 0
+        # 追加到历史表中
+        chis_data.append(y_hot_t)
+        # 保存结果
+        temp_reuslt += y_hot_t
+    noise = random.gauss(0, sigma)
+    noise = math.fabs(noise)
+    # 求一个浮点数的地板，就是求一个最接近它的整数 ceil向上取整
+    result.append(int(math.floor(temp_reuslt) + noise))
+    return result
+
+
+def predict_model6(his_data, date_range_size, vm_type):  # 霍尔特线性趋势法
+    '''
+    预测方案 6 霍尔特线性趋势法
+    :param his_data: 真实的历史数据出现次数表
+    :param date_range_size: 需要预测的长度
+    :return: 返回结果
+    '''
+    # 历史天数
+    chis_data = copy.deepcopy(his_data['value'])
+    # 历史天数
+    cal_len = len(chis_data)
+    temp_reuslt = 0.0
+    result = []
+    #
+    sigma = 0.2
+
+    # 衰减值
+    alpha = 0.2
+    # 趋势
+    beta = 0.0
+    # 权重 75.21
+    h = 1.75
+
+    y_hot_t = 0.0
+    l_t = 0.2
+    b_t = 0.2
+
+    # 初始trend
+    pre_b_t = 0.0
+    # 初始化level
+    pre_l_t = 0.0
+
+    for rept in range(date_range_size):  # 预测天数范围
+
+        # 遍历历史记录
+        for i in range(1, len(chis_data)):  # t+1开始
+            # 更新level trend
+            pre_l_t = l_t
+            pre_b_t = b_t
+            # step1 computer level
+            l_t = alpha * chis_data[i - 1] + (1 - alpha) * (pre_l_t + pre_b_t)
+            # step2 computer trend
+            b_t = beta * (l_t - pre_l_t) + (1 - beta) * b_t
+            # step3
+            y_hot_t = l_t + h * b_t
+            if y_hot_t < 0:
+                y_hot_t = 0
+        # 追加到历史表中
+        chis_data.append(y_hot_t)
+        # 保存结果
+        temp_reuslt += y_hot_t
+    noise = random.gauss(0, sigma)
+    noise = math.fabs(noise)
+    # 求一个浮点数的地板，就是求一个最接近它的整数 ceil向上取整
+    result.append(int(math.floor(temp_reuslt) + noise))
     return result
 
 
@@ -266,8 +295,7 @@ def predict_model7(his_data,  # 某种类型的虚拟机的历史数据
                    date_range_size, vm_type):  # 需要预测的长度
 
     '''
-    预测方案七,对若干星期前同一天数据求平均
-    his_data:['time':[时间标签],'value':[值]]
+    预测方案七 对若干星期前同一天数据求平均
     '''
 
     n = 10  # 边长数
@@ -313,10 +341,7 @@ def predict_model7(his_data,  # 某种类型的虚拟机的历史数据
 def predict_model8(his_data,  # 某种类型的虚拟机的历史数据
                    date_range_size, vm_type):  # 需要预测的长度
 
-    '''
-        预测方案七,对若干星期前同一天数据求平均
-        his_data:['time':[时间标签],'value':[值]]
-        '''
+    #无noise
 
     n = 10  # 边长数
     sigma = 0.5
@@ -799,10 +824,7 @@ def predict_model14(his_data, date_range_size, vm_type):  # Holt-Winters法
 def predict_model15(his_data,  # 某种类型的虚拟机的历史数据
                     date_range_size, vm_type):  # 需要预测的长度
 
-    '''
-    对若干星期前同一天数据求平均
-    his_data:['time':[时间标签],'value':[值]]
-    '''
+
 
     n = 10  # 边长数10
     sigma = 0.5
@@ -851,10 +873,7 @@ def predict_model15(his_data,  # 某种类型的虚拟机的历史数据
 def predict_model16(his_data,  # 某种类型的虚拟机的历史数据
                     date_range_size, vm_type):  # 需要预测的长度
 
-    '''
-       预测方案七,对若干星期前同一天数据求平均
-       his_data:['time':[时间标签],'value':[值]]
-       '''
+   
 
     n = 10  # 边长数 10
     sigma = 0.5
@@ -905,10 +924,7 @@ def predict_model16(his_data,  # 某种类型的虚拟机的历史数据
 
 def predict_model17(his_data,  # 某种类型的虚拟机的历史数据
                     date_range_size, vm_type):  # 需要预测的长度
-    '''
-       预测方案七,对若干星期前同一天数据求平均
-       his_data:['time':[时间标签],'value':[值]]
-       '''
+    
 
     n = 2  # 边长数2  83.075
     sigma = 0.5
@@ -956,10 +972,7 @@ def predict_model17(his_data,  # 某种类型的虚拟机的历史数据
 def predict_model18(his_data,  # 某种类型的虚拟机的历史数据
                     date_range_size, vm_type):  # 需要预测的长度
 
-    '''
-   预测方案七,对若干星期前同一天数据求平均
-   his_data:['time':[时间标签],'value':[值]]
-   '''
+    
 
     n = 10  # 边长数10 83.11
     sigma = 0.5
