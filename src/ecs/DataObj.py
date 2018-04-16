17ce8817fcdae195ef048373850b5af932631a7c
Joahluo <luojie@hnu.edu.cn>
2018-04-16 18:38:46 +0800

    smooth

--- src/ecs/DataObj.py
+++ src/ecs/DataObj.py
@@ -119,9 +119,9 @@ class DataObj(object):
             self.date_range_size = td.days
         self.data_range = [_st, _et]
 
-        self.CPU = self.mp_type_list["H"]["CPU"]
-        self.MEM = self.mp_type_list["H"]["MEM"]
-        self.HDD = self.mp_type_list["H"]["HDD"]
+        self.CPU = self.mp_type_list["G"]["CPU"]
+        self.MEM = self.mp_type_list["G"]["MEM"]
+        self.HDD = self.mp_type_list["G"]["HDD"]
 
     def set_his_data(self, origin_train_data, predict_time_grain):
         if (origin_train_data is None) or \
