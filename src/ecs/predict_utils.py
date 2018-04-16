1e676aafe96acf9e43c434f6d3a59c75455875f2
Joahluo <luojie@hnu.edu.cn>
2018-04-16 18:40:17 +0800

    smooth

--- src/ecs/predict_utils.py
+++ src/ecs/predict_utils.py
@@ -124,7 +124,7 @@ def predict_all(dataObj):
     #     predict_func = predict_model.model4_used_func  # model4_used_func 77.156
     #################################################MAX-SCORE##################################################
 
-    predict_func = predict_model.model8_used_func
+    predict_func = predict_model.model1_used_func
     vmtype_avage_v=3
 
     # predict_func = predict_model.model9_used_func
