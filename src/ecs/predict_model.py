bf1b6b34f0f28fbdbaf40bff88620daf6a60892d
Joahluo <luojie@hnu.edu.cn>
2018-04-18 17:25:49 +0800

    fix gaussian filter

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -859,8 +859,8 @@ def predict_model15(his_data,  # 某种类型的虚拟机的历史数据
     n = 3  # 边长数3
     sigma = 0.5
     # 放大系数
-    enlarge = 1.43  # 151
-    beta = 1.1  # 2
+    enlarge = 1.38  # 151
+    beta = 2  # 2
     back_week = 1  # 1
     chis_data = copy.deepcopy(his_data['value'])
     cal_len = len(chis_data)
