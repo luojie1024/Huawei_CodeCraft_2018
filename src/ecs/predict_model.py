fedf39080183aea5c095995b04b30bd0936fd926
Joahluo <luojie@hnu.edu.cn>
2018-04-18 16:52:40 +0800

    fix gaussian filter

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -58,11 +58,11 @@ def predict_model2(his_data, dataObj, vm_type):
     #获取放大权重
     # count_weight=dataObj.get_count_weight(vm_type)
 
-    n = 1  # 边长数3
+    n = 3  # 边长数3
     sigma = 0.5
     # 放大系数
-    enlarge = 1.54 # 151
-    beta = 2  # 2
+    enlarge = 1.5 # 151
+    beta = 2 # 2
     back_week = 1  # 1
     chis_data = copy.deepcopy(his_data['value'])
     cal_len = len(chis_data)
@@ -849,64 +849,24 @@ def predict_model14(his_data, dataObj, vm_type):  # Holt-Winters法
 ###################对若干星期前同一天数据求平均######################
 def predict_model15(his_data,  # 某种类型的虚拟机的历史数据
                     dataObj, vm_type):  # 需要预测的长度
+    # 无noise
+    # 需要预测的天数
     date_range_size = dataObj.date_range_size
-    n = 10  # 边长数10
-    sigma = 0.5
 
-    back_week = 1
+    # 获取放大权重
+    # count_weight=dataObj.get_count_weight(vm_type)
+
+    n = 3  # 边长数3
+    sigma = 0.5
+    # 放大系数
+    enlarge = 1.38  # 151
+    beta = 2  # 2
+    back_week = 1  # 1
     chis_data = copy.deepcopy(his_data['value'])
     cal_len = len(chis_data)
 
     result = []
-    for rept in range(date_range_size):  # 预测天数范围
-        day_avage = 0.0
-        cot_week = 0
-        for i in range(1, back_week + 1):
-            index = i * 7
-            if index <= cal_len:
-                day_tmp = chis_data[-index] * n
-                cot_day = n
-                cot_week += 1
-                for j in range(1, n):
-                    tmp = (n - j) / 2.0
-                    day_tmp += chis_data[-index + j] * tmp
-                    cot_day += tmp
-                    if index + j <= cal_len:
-                        day_tmp += chis_data[-index - j] * tmp
-                        cot_day += tmp
-                    else:
-                        continue
-                day_avage += day_tmp / cot_day
-            else:
-                break
-        # if cot_week != 0:
-        #     day_avage = day_avage * 1.0 / cot_week  # 注意报错
-        # if is_noise:
-        #     noise = random.gauss(0, sigma)
-        #     noise = math.fabs(noise)
-        #     day_avage = int(math.ceil(day_avage + noise))
-        # else:
-        #     day_avage = int(math.ceil(day_avage))
-        day_avage = int(math.ceil(day_avage))
-        chis_data.append(day_avage)
-        result.append(day_avage)
-
-    return result
-
-
-def predict_model16(his_data,  # 某种类型的虚拟机的历史数据
-                    dataObj, vm_type):  # 需要预测的长度
-    date_range_size = dataObj.date_range_size
-    n = 10  # 边长数 10
-    sigma = 0.5
-
-    beta = 2.0
-
-    back_week = 1
-    chis_data = copy.deepcopy(his_data['value'])
-    cal_len = len(chis_data)
-
-    result = []
+    temp_result = 0
     for rept in range(date_range_size):  # 预测天数范围
         day_avage = 0.0
         cot_week = 0
@@ -928,18 +888,73 @@ def predict_model16(his_data,  # 某种类型的虚拟机的历史数据
                 day_avage += day_tmp / cot_day
             else:
                 break
-        if cot_week != 0:
+        if cot_week != 0:  # 直接平均  --> 改进成指数平均
             day_avage = day_avage * 1.0 / cot_week  # 注意报错
-        if is_noise:
-            noise = random.gauss(0, sigma)
-            noise = math.fabs(noise)
-            day_avage = int(math.ceil(day_avage + noise))
-        else:
-            day_avage = int(math.ceil(day_avage))
-
+        # noise = random.gauss(0, sigma)
+        # noise = math.fabs(noise)
+        # day_avage = int(math.ceil(day_avage + noise))
+        # 系数放大,修正高斯效果
+        day_avage = day_avage * enlarge
         day_avage = int(math.ceil(day_avage))
         chis_data.append(day_avage)
-        result.append(day_avage)
+        temp_result += day_avage
+    result.append(temp_result)
+
+    return result
+
+
+def predict_model16(his_data,  # 某种类型的虚拟机的历史数据
+                    dataObj, vm_type):  # 需要预测的长度
+    # 无noise
+    # 需要预测的天数
+    date_range_size = dataObj.date_range_size
+
+    # 获取放大权重
+    # count_weight=dataObj.get_count_weight(vm_type)
+
+    n = 10  # 边长数3
+    sigma = 0.5
+    # 放大系数
+    enlarge = 1.38  # 151
+    beta = 2  # 2
+    back_week = 1  # 1
+    chis_data = copy.deepcopy(his_data['value'])
+    cal_len = len(chis_data)
+
+    result = []
+    temp_result = 0
+    for rept in range(date_range_size):  # 预测天数范围
+        day_avage = 0.0
+        cot_week = 0
+        for i in range(1, back_week + 1):
+            index = i * 7
+            if index <= cal_len:
+                day_tmp = chis_data[-index] * n
+                cot_day = n
+                cot_week += 1
+                for j in range(1, n):
+                    tmp = (n - j) / beta
+                    day_tmp += chis_data[-index + j] * tmp
+                    cot_day += tmp
+                    if index + j <= cal_len:
+                        day_tmp += chis_data[-index - j] * tmp
+                        cot_day += tmp
+                    else:
+                        continue
+                day_avage += day_tmp / cot_day
+            else:
+                break
+        if cot_week != 0:  # 直接平均  --> 改进成指数平均
+            day_avage = day_avage * 1.0 / cot_week  # 注意报错
+        # noise = random.gauss(0, sigma)
+        # noise = math.fabs(noise)
+        # day_avage = int(math.ceil(day_avage + noise))
+        # 系数放大,修正高斯效果
+        day_avage = day_avage * enlarge
+        day_avage = int(math.ceil(day_avage))
+        chis_data.append(day_avage)
+        temp_result += day_avage
+    result.append(temp_result)
 
     return result
 
