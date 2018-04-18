9d1e808cc69ce9f0470c767be8cf0d63c98c5745
Joahluo <luojie@hnu.edu.cn>
2018-04-18 12:04:41 +0800

    test

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -151,7 +151,7 @@ def predict_model4(his_data, date_range_size, vm_type):  # 霍尔特线性趋势
     # 趋势
     beta = 0.0
     # 权重 3    趋势权重     3.5-66.503  3.6-65.85
-    h = 3.55
+    h = 3.47
 
     y_hot_t = 0.0
     l_t = 0.2
