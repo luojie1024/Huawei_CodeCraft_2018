733e7fb6545a0eae7d4624959313b0f9def04438
Joahluo <luojie@hnu.edu.cn>
2018-04-19 15:29:06 +0800

    fix gaussian bug

--- src/ecs/predict_utils.py
+++ src/ecs/predict_utils.py
@@ -133,10 +133,10 @@ def predict_all(dataObj):
 
 
     if gap_time>1:
-        predict_func = predict_model2.model2_used_func
+        predict_func = predict_model2.model1_used_func
 
     # 3x3填充方案
-    vmtype_avage_v = 7
+    vmtype_avage_v = 6
 
     # vmtype_avage_v = 6
 
