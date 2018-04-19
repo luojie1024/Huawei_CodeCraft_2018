4bd5390264cfa12e75f3fb527fac971142119fbe
Joahluo <luojie@hnu.edu.cn>
2018-04-19 14:01:08 +0800

    fix gaussian bug

--- src/ecs/predict_model.py
+++ src/ecs/predict_model.py
@@ -941,7 +941,7 @@ def predict_model16(his_data,  # 某种类型的虚拟机的历史数据
     n = 6  # 边长数3
     sigma = 0.5
     # 放大系数
-    enlarge = 1.58  # 1.58  	77.164
+    enlarge = 1.0  # 1.58  	77.164
     beta = 2  # 2
     back_week = 1  # 1
     chis_data = copy.deepcopy(his_data['value'])
