0c4489461d3c4a18130af269298b37067b9451d5
Joahluo <luojie@hnu.edu.cn>
2018-04-19 13:16:19 +0800

    fix gaussian bug

--- src/ecs/DataObj.py
+++ src/ecs/DataObj.py
@@ -535,7 +535,7 @@ class DataObj(object):
         ########################原始数据矩阵############################
 
         # 过滤 filter='average'   filter='gaussian'高斯滤波
-        result['value'] = to_filter(orign_martix_data, filter='gaussian', sigma=7, orgin_data_size=self.train_day_count,
+        result['value'] = to_filter(orign_martix_data, filter='gaussian', sigma=5, orgin_data_size=self.train_day_count,
                                     avg_count=avg_count)
 
         return result
