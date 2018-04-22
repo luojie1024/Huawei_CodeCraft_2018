dfe7d002fb2f29164a5cbe4bd381b6154136fa09
Joahluo <luojie@hnu.edu.cn>
2018-04-22 14:57:35 +0800

    拾取方案pack2

--- src/ecs/predict_utils.py
+++ src/ecs/predict_utils.py
@@ -132,13 +132,13 @@ def predict_all(dataObj):
 
 
     if gap_time==1:
-        predict_func = predict_model2.model1_used_func
+        predict_func = predict_model2.model2_used_func
 
     # predict_func = predict_model2.model3_used_func
     # 3x3填充方案
     # vmtype_avage_v = 6
 
-    vmtype_avage_v = 6
+    vmtype_avage_v = 7
 
     # predict_func = predict_model.model9_used_func
     # vmtype_avage_v=3
