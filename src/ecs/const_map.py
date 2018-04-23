e79d31178b20770bd91e28af3b5ba6f0d7028fdd
Joahluo <luojie@hnu.edu.cn>
2018-04-23 11:17:00 +0800

    拾取方案pack2+分段预测+调参

--- src/ecs/const_map.py
+++ src/ecs/const_map.py
@@ -150,14 +150,14 @@ PREDICT_MODEL21_WEIGHTS = {  # 84.543 #有  ##无
     'flavor9': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},
 
     'flavor10': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},##
-    'flavor11': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},
-    'flavor12': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},
+    'flavor11': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.55},
+    'flavor12': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.55},
 
     'flavor13': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},##
     'flavor14': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},##
-    'flavor15': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.55},
+    'flavor15': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},##
 
-    'flavor16': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.55},
+    'flavor16': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},##
     'flavor17': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},##
     'flavor18': {'n': 10, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.49},##
 }
