0ad086fd64fd64860656fcdba7c559dad199d3ff
Joahluo <luojie@hnu.edu.cn>
2018-04-23 11:41:46 +0800

    拾取方案pack2+分段预测+调参

--- src/ecs/const_map.py
+++ src/ecs/const_map.py
@@ -85,7 +85,7 @@ VM_MEM_QU = {  # MEM,CPU少的优先,从后往前
 
 # 平均模型，权重 
 PREDICT_MODEL1_WEIGHTS = {
-    'flavor1': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.1},
+    'flavor1': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.01},
     'flavor2': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1},
     'flavor3': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1},
 
@@ -94,7 +94,7 @@ PREDICT_MODEL1_WEIGHTS = {
     'flavor6': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1},
 
     'flavor7': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1},
-    'flavor8': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1},
+    'flavor8': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1.01},
     'flavor9': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1},
 
     'flavor10': {'n': 3, 'back_week': 1, 'beta': 2.0, 'enlarge': 1},
