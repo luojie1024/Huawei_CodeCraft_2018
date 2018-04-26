8ca61c8c96613610d98d8a3d1c74ee3692afb7b7
Joahluo <luojie@hnu.edu.cn>
2018-04-26 13:41:07 +0800

    use_smooth fix

--- src/ecs/predict.py
+++ src/ecs/predict.py
@@ -49,7 +49,7 @@ is_parameter_search = False
 # 使用深度学习模型
 is_deeplearing = False
 use_smooth = False
-use_search_maximum = True #86.277   83.026
+use_search_maximum = False #86.277   83.026
 use_search_u_m_maximum = False
 
 
