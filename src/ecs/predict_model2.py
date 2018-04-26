87685022c1e13863d78297b6d42295bd214f1326
Joahluo <luojie@hnu.edu.cn>
2018-04-26 13:14:04 +0800

    use_smooth fix

--- src/ecs/predict_model2.py
+++ src/ecs/predict_model2.py
@@ -388,7 +388,7 @@ def predict_model5(his_data, dataObj, vm_type):
 
     if dataObj.date_range_size == 7 and dataObj.vm_types_size == 5:  # 预测时间7天,间隔1天(连续)
         weight = PREDICT_MODEL1_WEIGHTS[vm_type]
-    elif dataObj.date_range_size == 14 and dataObj.gap_time == 8 and dataObj.vm_types_size == 6:  # 预测时间14天,间隔7天(=8)
+    elif dataObj.date_range_size == 14 and dataObj.gap_time == 8 and dataObj.vm_types_size == 5:  # 预测时间14天,间隔7天(=8)
         weight = PREDICT_MODEL21_WEIGHTS[vm_type]
     # if dataObj.gap_time == 1:  # 无间隔 7天预测
     #     weight = PREDICT_MODEL1_WEIGHTS[vm_type]
